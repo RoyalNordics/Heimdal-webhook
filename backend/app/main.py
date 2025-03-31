a8c497581b11774f05d03f8a9e38ad9e19de0d44
Thomas Flyttov Møller <thomas.flyttov@hotmail.com>
2025-03-31 18:11:02 +0000

    Include api_router in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
-from backend.app.services.roo.roo_core import handle_action
+from fastapi import FastAPI
+from backend.app.api.api import api_router
 
 app = FastAPI()
 
+app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/")
 def root():
